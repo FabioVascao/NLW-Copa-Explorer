f9ae01e5e7949d3cd9517c94514fe39b0d21f6b7
Fabio de Sousa Santos <109177231+FabioVascao@users.noreply.github.com>
2022-11-04 10:45:11 -0300

    logos att

--- main.js
+++ main.js
@@ -22,8 +22,8 @@ function createCard(date, day, games) {
 }
 
 document.querySelector('#cards').innerHTML = 
-     createCard("20/11", "domingo", createGame('qatar', '13:00', 'ecuador')) +
-     createCard("21/11", "segunda", createGame('england', '10:00', 'iran') + createGame('senegal', '13:00', 'netherlands') + createGame('unitedstates', '16:00', 'wales')) +
-     createCard("24/11", "quinta", createGame('brazil', '16:00', 'serbia')) +
-     createCard("28/11", "segunda", createGame('brazil', '13:00', 'switzerland') + createGame('portugal', '16:00', 'uruguay')) + 
-      createCard("02/12", "sexta", createGame('brazil', '16:00', 'cameroon'))
+    createCard("20/11", "domingo", createGame('qatar', '13:00', 'ecuador')) + 
+    createCard("28/11", "segunda", createGame('brazil', '13:00', 'switzerland') + createGame('portugal', '16:00', 'uruguay')) +
+    createCard("24/11", "quinta", createGame('brazil', '16:00', 'serbia')) +
+    createCard("28/11", "segunda", createGame('brazil', '13:00', 'switzerland') + createGame('portugal', '16:00', 'uruguay')) + 
+    createCard("02/12", "sexta", createGame('brazil', '16:00', 'cameroon'))
